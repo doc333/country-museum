462c5dbd0a9fdbf7be0c3d0e5cdc0cffc0721d4d
unknown <girard.yorick@gmail.com>
2015-08-05 11:17:00 +0200

    Ajout des flèche directionnelles

--- app/src/main/java/com/example/md2/magicmuseum/MuseumActivity.java
+++ app/src/main/java/com/example/md2/magicmuseum/MuseumActivity.java
@@ -1,7 +1,14 @@
 package com.example.md2.magicmuseum;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 /**
  * Created by admin on 04/08/2015.
  */
-public class MuseumActivity {
+public class MuseumActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_museum);
+    }
 }
