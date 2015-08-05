e2e84d5baf9bb8ec07fea49ccf03481407ff1854
unknown <girard.yorick@gmail.com>
2015-08-05 11:35:59 +0200

    Ajout d'un bouton pour accéder au musée

--- app/src/main/java/com/example/md2/magicmuseum/MainActivity.java
+++ app/src/main/java/com/example/md2/magicmuseum/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,4 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+        public void startMuseum(View view)
+        {
+                Intent intent = new Intent(MainActivity.this, MuseumActivity.class);
+                startActivity(intent);
+        }
 }
