284a920cbdcc76d1a9a098f3340b874c3b896b90
unknown <girard.yorick@gmail.com>
2015-08-06 11:20:20 +0200

    Afficher les drapeaux en fonction de leur id

--- app/src/main/java/com/example/md2/magicmuseum/MuseumActivity.java
+++ app/src/main/java/com/example/md2/magicmuseum/MuseumActivity.java
@@ -2,13 +2,37 @@ package com.example.md2.magicmuseum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.md2.magicmuseum.data.Country;
+
+import com.example.md2.magicmuseum.data.CountryList;
+
+import java.util.ListIterator;
+
 /**
  * Created by admin on 04/08/2015.
  */
 public class MuseumActivity extends AppCompatActivity {
 
+    private CountryList countries;
+    private int currentCountryIndex = 0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_museum);
+        countries = new CountryList();
+
+        ListIterator<Country> iterator = countries.getCountryList().listIterator(currentCountryIndex);
+        int i = 0;
+        while(i < 3 && iterator.hasNext())
+        {
+            i++;
+            Country c = iterator.next();
+            countries.getCountryList().iterator().next();
+            int countryId = c.getId();
+            ImageView image = (ImageView) findViewById(this.getResources().getIdentifier("imageView" + i, "id", this.getPackageName()));
+            image.setImageResource(this.getResources().getIdentifier("big_" + countryId, "mipmap", this.getPackageName()));
+        }
     }
 }
