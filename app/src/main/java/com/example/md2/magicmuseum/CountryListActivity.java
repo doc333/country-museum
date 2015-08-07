eb03b937462f6c3758528a9ede1626d1d6f90cfa
doc333 <mangold.loic@gmail.com>
2015-08-07 09:48:03 +0200

    m

--- app/src/main/java/com/example/md2/magicmuseum/CountryListActivity.java
+++ app/src/main/java/com/example/md2/magicmuseum/CountryListActivity.java
@@ -3,6 +3,8 @@ package com.example.md2.magicmuseum;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -37,5 +39,35 @@ public class CountryListActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_country_listview) {
+            Intent intent = new Intent(CountryListActivity.this, CountryListActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_museum) {
+            Intent intent = new Intent(CountryListActivity.this, MuseumActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
